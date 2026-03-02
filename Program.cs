5d979fed92f30b207a6013f1da32f05000eac83d
mariapinholate <mariapinholate@gmail.com>
2026-03-02 20:26:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*int num1, num2, soma; 
+
+Console.WriteLine("digite um numero: ");
+num1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Digite outro numero: ");
+num2 = int.Parse (Console.ReadLine());
+soma = num1 + num2;
+Console.WriteLine("soma: {0} \n", soma);*/
+
+int x, y;
+Console.WriteLine("digite um numero: ");
+x = int.Parse(Console.ReadLine());
+y = 2*x;
+Console.WriteLine("dobro: " + y);
+Console.ReadKey();
+
+
+
+
